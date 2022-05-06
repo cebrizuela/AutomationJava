b322b11ca803093719ddbe68805a3a86be1e4d10
cebrizuela2020@gmail.com <cebrizuela2020@gmail.com>
2022-05-06 15:01:10 -0300

    clase de los comandos

--- src/main/java/CourseAppium/myCode/driverCommands/LockAndUnloackDevice.java
+++ src/main/java/CourseAppium/myCode/driverCommands/LockAndUnloackDevice.java
@@ -1,2 +1,21 @@
-package CourseAppium.myCode.driverCommands;public class LockAndUnloackDevice {
+package CourseAppium.myCode.driverCommands;
+
+import CourseAppium.myCode.CreateDriverSession;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LockAndUnloackDevice {
+
+       public static void main(String[] args) throws Exception {
+        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        ((AndroidDriver) driver).lockDevice();
+        System.out.println(((AndroidDriver) driver).isDeviceLocked());
+        ((AndroidDriver) driver).unlockDevice();
+
+    }
+
 }
